942b6ba777f33966eb32446c60ceed17cda4fa5e
Mokl <moise.lungeni@gmail.com>
2017-04-17 19:20:12 +0200

    Update DBmanager, DB and diagram

--- ticketing_app/DB/ticketing_app.sql
+++ ticketing_app/DB/ticketing_app.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 17 Avril 2017 à 00:54
+-- Généré le :  Lun 17 Avril 2017 à 18:44
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -31,6 +31,20 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `id` varchar(20) NOT NULL,
   `password` varchar(10) NOT NULL,
   `access_code` int(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `nom` varchar(30) NOT NULL,
+  `descrition` varchar(140) NOT NULL DEFAULT 'none',
+  `prix` float NOT NULL,
+  `quantite` int(11) NOT NULL DEFAULT '0',
+  `vendu` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
--- ticketing_app/app/src/main/java/be/ecam/ticketing/ticketing_app/DatabaseManager.java
+++ ticketing_app/app/src/main/java/be/ecam/ticketing/ticketing_app/DatabaseManager.java
@@ -84,9 +84,9 @@ public class DatabaseManager
             conn = DriverManager.getConnection(conn_string[0],conn_string[1],conn_string[2]);
             Statement stmt = conn.createStatement();
             String query = "INSERT INTO user VALUES('"+info[0]+"','"+info[1]+"','"+info[2]+"','"+info[3]+"','"+info[4]+"','"+info[5]+"','"+info[6]+"');";
-            ResultSet result = stmt.executeQuery(query);
+            //ResultSet result = stmt.executeQuery(query);
 
-            result.close();
+            //result.close();
             stmt.close();
             conn.close();
             return true;
@@ -97,4 +97,98 @@ public class DatabaseManager
         }
     }
 
+    public boolean CreateAdmin(String[] info[])
+    {
+        try
+        {
+            conn = DriverManager.getConnection(conn_string[0],conn_string[1],conn_string[2]);
+            Statement stmt = conn.createStatement();
+            String query = "INSERT INTO employee VALUES('"+info[0]+"','"+info[1]+"','"+info[2]+"','"+info[3]+");";
+            //ResultSet result = stmt.executeQuery(query);
+
+            //result.close();
+            stmt.close();
+            conn.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    public boolean Pay(double montant, String[] info)
+    {
+        try
+        {
+            conn = DriverManager.getConnection(conn_string[0],conn_string[1],conn_string[2]);
+            Statement stmt = conn.createStatement();
+            String montant_str = Double.toString(montant);
+            String query = "UPDATE user SET solde = 'solde-"+montant_str+"' WHERE id='"+info[0]+"' AND password='"+info[1]+"';";
+            stmt.executeQuery(query);
+
+            stmt.close();
+            conn.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    public boolean Recharge(double montant, String[] info)
+    {
+        try
+        {
+            conn = DriverManager.getConnection(conn_string[0],conn_string[1],conn_string[2]);
+            Statement stmt = conn.createStatement();
+            String montant_str = Double.toString(montant);
+            String query = "UPDATE user SET solde = 'solde+"+montant_str+"' WHERE id='"+info[0]+"' AND password='"+info[1]+"';";
+            stmt.executeQuery(query);
+
+            stmt.close();
+            conn.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    public boolean SetProduct(String[][] info,int nb_product)
+    {
+        try
+        {
+            conn = DriverManager.getConnection(conn_string[0],conn_string[1],conn_string[2]);
+            PreparedStatement stmt = null;
+            int i =0 ;
+            conn.setAutoCommit(false);
+            String query = "INSERT INTO product VALUES(?,?,?,?,?);";
+
+            conn.prepareStatement(query);
+            for(i=0;i<nb_product-1;i++)
+            {
+                stmt.setObject(1,info[i][0],Types.VARCHAR);
+                stmt.setObject(2,info[i][1],Types.VARCHAR);
+                stmt.setObject(3,info[i][2],Types.FLOAT);
+                stmt.setObject(4,info[i][3],Types.INTEGER);
+                stmt.setObject(5,info[i][4],Types.INTEGER);
+                stmt.execute();
+            }
+            conn.commit();
+
+            //ResultSet result = stmt.executeQuery(query);
+
+            //result.close();
+            stmt.close();
+            conn.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
 }
