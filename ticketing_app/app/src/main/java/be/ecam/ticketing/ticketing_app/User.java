4e7dfd1f833e706f87eec2a1653ccd4e8b9b66e1
Juan Barrera <jbarreravega@users.noreply.github.com>
2017-04-25 09:12:17 +0200

    Admin class, basic methods for User and Admin

--- ticketing_app/app/src/main/java/be/ecam/ticketing/ticketing_app/User.java
+++ ticketing_app/app/src/main/java/be/ecam/ticketing/ticketing_app/User.java
@@ -6,4 +6,43 @@ package be.ecam.ticketing.ticketing_app;
  */
 
 public class User {
+    private String nom, prenom, mail;
+    private int ID, age, pswd, droit;
+    private Compte compte;
+
+    public User(String nom, String prenom, String mail,
+                int ID, int age, int pswd, int droit, Compte compte){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mail = mail;
+        this.ID = ID;
+        this.age = age;
+        this.pswd = pswd;
+        this.droit = droit;
+        this.compte = compte;
+    }
+
+    public String getFirstName(){
+        return prenom;
+    }
+
+    public String getForeName(){
+        return nom;
+    }
+
+    public String getMail(){
+        return mail;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void changePassword(int pswd){
+        this.pswd = pswd;
+    }
 }
