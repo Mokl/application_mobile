c4252b2e391ca57fcba1e4573a17453b10eee0af
Mokl <moise.lungeni@gmail.com>
2017-04-28 09:20:59 +0200

    Bugs fix v2 By Louis Engelen

--- ticketing_app/app/src/main/java/be/ecam/ticketing/ticketing_app/Settingsfragment.java
+++ ticketing_app/app/src/main/java/be/ecam/ticketing/ticketing_app/Settingsfragment.java
@@ -1,6 +1,7 @@
 package be.ecam.ticketing.ticketing_app;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.v7.preference.PreferenceDialogFragmentCompat;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
@@ -10,7 +11,7 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat
 {
-
+    @Override
     public void onCreatePreferences(Bundle bundle,String s)
     {
         addPreferencesFromResource (R.xml.preferences);
